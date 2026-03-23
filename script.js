8aef27e119ea5fd1ce445895420bbbbb5f71c42f
yunadata <yunadatagit@gmail.com>
2026-03-23 14:13:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -58,3 +58,24 @@ document.addEventListener('DOMContentLoaded', () => {
         observer.observe(card);
     });
 });
+
+
+
+
+
+
+
+// Function to expand custom videos to fullscreen
+function openFullscreen(videoId) {
+    const elem = document.getElementById(videoId);
+    
+    if (!elem) return;
+
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+}
